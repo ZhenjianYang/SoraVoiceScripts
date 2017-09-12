87787d78eb06705b5b66677449043743be20ad8a
Ken Yang <ZJ.Yang@qq.com>
2017-09-12 13:51:50 +0800

    Update bats

--- en.fc/0.Clean.bat
+++ en.fc/0.Clean.bat
@@ -1,9 +1,13 @@
 @echo off
-title %~n0%
 
 set curp=%~dp0
 set p=%~p0
 set p=%p:\= %
 for %%a in (%p%) do set curd=%%a
+set tmp=%curp%\tmp
+
+title %curd% %~n0%
+
+call "%curp%\..\Common.bat"
 
 rd /S /Q "%curp%\tmp"
--- en.fc/5.Pack.bat
+++ en.fc/5.Pack.bat
@@ -1,11 +1,12 @@
 @echo off
-title %~n0%
 
 set curp=%~dp0
 set p=%~p0
 set p=%p:\= %
 for %%a in (%p%) do set curd=%%a
 set tmp=%curp%\tmp
+
+title %curd% %~n0%
 
 call "%curp%\..\Common.bat"
 
