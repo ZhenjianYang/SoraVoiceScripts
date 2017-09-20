23f277ce597c1c40bd20a71ed6a819178b65dfb6
Ken Yang <ZJ.Yang@qq.com>
2017-09-20 11:25:02 +0800

    Update Import.bat

--- cn.fc/2.Import.bat
+++ cn.fc/2.Import.bat
@@ -10,4 +10,13 @@ title %curd% %~n0%
 
 call "%curp%\..\Common.bat"
 
-"%dir_tools%\SoraInputTalk.exe" -c p "%curp%\py"  "%tmp%\v.out.py" "%tmp%\v.py" "%tmp%\report_import.txt"
+set py=%tmp%\py
+md "%py%"
+
+setlocal enabledelayedexpansion
+for %%i in ("%curp%\py\*.py") do (
+set a=%%~ni
+for /f "tokens=1 delims=." %%j in ("!a!") do copy /Y "%%i" "%py%\%%j.py"
+)
+
+"%dir_tools%\SoraInputTalk.exe" -c p "%py%"  "%tmp%\v.out.py" "%tmp%\v.py" "%tmp%\report_import.txt"
