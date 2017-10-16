9f528ae50ae17d45aef2b1f05b161b8c022d831c
Ken Yang <ZJ.Yang@qq.com>
2017-10-16 10:33:18 +0800

    Update bat

--- CleanAll.bat
+++ CleanAll.bat
@@ -1,4 +1,6 @@
 @echo off
 
+cd %~dp0
+
 for %%b in (cn.fc en.fc en.sc) do .\%%b\0.Clean.bat
 
--- DoAll.bat
+++ DoAll.bat
@@ -1,5 +1,7 @@
 @echo off
 
+cd %~dp0
+
 for %%b in (cn.fc en.fc en.sc) do start .\%%b\DoAll.bat
 
 exit
