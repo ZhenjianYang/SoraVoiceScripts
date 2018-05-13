40595af962554e5f27a8e0bc1301f6a86a9892b1
Zhenjian Yang <zhenjian.c.yang@gmail.com>
2018-05-13 21:51:33 +0900

    en.3rd: minigame in Sun Door 4

--- en.3rd/6.GenMG.bat
+++ en.3rd/6.GenMG.bat
@@ -17,4 +17,5 @@ mkdir "%sndir%"
 
 for %%i in (%vqdir%\T_QUIZ*.txt) do py "%dir_tools%\Txt2Quiz.py" --cp=ms932 "%%i" "%sndir%"
 for %%i in (%vqdir%\T_FISH*.txt) do py "%dir_tools%\Txt2Fish.py" --cp=ms932 "%dir_en3rd%\%%~ni._DT" "%%i" "%sndir%"
+for %%i in (%vqdir%\Z_POKER*.txt) do copy /Y "%%i" "%sndir%/%%~ni._DT"
 
